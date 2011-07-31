55dd4a6accec0bf63ca6f2360caecb0b4fd17190
Nicholas Mayne <Jetski5822@hotmail.com>
2011-07-31 11:28:31 +0100

    missed file

---
+++ Models/Language.cs
@@ -0,0 +1,14 @@
+﻿namespace NGM.TwitterFollowButton.Models {
+    public enum Language {
+        English,
+        French,
+        German,
+        Italian,
+        Japanese,
+        Korean,
+        Portuguese,
+        Russian,
+        Spanish,
+        Turkish
+    }
+}
